7302dbad850bd85399d5922a977e26cb306adf1d
Msk <118117161+Mskxn@users.noreply.github.com>
2023-02-28 19:29:49 +0800

    script

--- script/patch.sh
+++ script/patch.sh
@@ -3,5 +3,6 @@ SRCPATH=$GOROOT/src
 
 cp ./patch/runtime/chan.go.patch $SRCPATH/runtime/chan.go
 cp ./patch/sync/mutex.go.patch $SRCPATH/sync/mutex.go
+cp ./patch/sync/rwmutex.go.patch $SRCPATH/sync/rwmutex.go
 cp ./patch/runtime/runtime2.go.patch $SRCPATH/runtime/runtime2.go
 
