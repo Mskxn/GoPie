d760e72626a3e50b6dec4e6de1b1bef447bdfd05
Msk <118117161+Mskxn@users.noreply.github.com>
2023-03-15 15:19:27 +0800

    Update sched.go

--- pkg/sched/sched.go
+++ pkg/sched/sched.go
@@ -1,167 +1,144 @@
 package sched
 
 import (
-	goleak "go.uber.org/goleak"
+	"fmt"
+	"go.uber.org/goleak"
+	"os"
 	"runtime"
+	"strings"
 	"sync"
-	"sync/atomic"
 	"testing"
 	"time"
-	"toolkit/pkg/log"
 )
 
-var event uint32
-var enablesend uint64
-var enablewait uint64
-var istraced uint64
+var event sync.Map
 var timeout time.Duration
 
-type IDMap struct {
-	m  map[uint64]uint64
-	mu sync.RWMutex
-}
-
-type LogEntry struct {
-	opid uint64
-	eid  uint32
-	addr uint64
-}
-
-var idmap IDMap
+var config *Config
+var cancel chan struct{}
 
 const (
 	debugSched = true
 )
 
-func (idm *IDMap) get(i uint64) uint64 {
-	idm.mu.RLock()
-	defer idm.mu.RUnlock()
-	if _, ok := idm.m[i]; !ok {
-		return i & ((uint64(1) << 32) - 1)
-	}
-	return idm.m[i]
-}
-
-func (idm *IDMap) set(mm map[uint64]uint64) {
-	idm.mu.Lock()
-	defer idm.mu.Unlock()
-	idm.m = make(map[uint64]uint64, 0)
-	for k, v := range mm {
-		idm.m[k] = v
-	}
-}
-
-const (
-	EMPTY = iota
-	LOCK
-	UNLOCK
-	RLOCK
-	RUNLOCK
-	SEND
-	RECV
-	CLOSE
-	WGADD
-	WGDONE
-	WGWAIT
-)
-
-const (
-	W_LOCK    = (1 << LOCK) | (1 << UNLOCK)
-	W_UNLOCK  = (1 << UNLOCK) | (1 << RUNLOCK)
-	S_LOCK    = 1 << LOCK
-	S_UNLOCK  = 1 << UNLOCK
-	S_RLOCK   = 1 << RLOCK
-	S_RUNLOCK = 1 << RUNLOCK
-	W_SEND    = (1 << SEND) | (1 << CLOSE)
-	W_RECV    = 1 << RECV
-	W_CLOSE   = (1 << CLOSE) | (1 << RECV)
-	S_SEND    = 1 << SEND
-	S_RECV    = 1 << RECV
-	S_CLOSE   = (1 << CLOSE) | (1 << SEND)
-)
-
 func init() {
+	config = NewConfig()
+	cancel = make(chan struct{})
 	timeout = time.Second * 2
-	if debugSched {
-		istraced = ^(uint64(0))
+}
+
+func (c *Config) waiter(i uint64) uint64 {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	if w, ok := config.waitmap[i]; ok {
+		return w
+	}
+	return 0
+}
+
+func (c *Config) sender(i uint64) uint64 {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	if w, ok := config.sendmap[i]; ok {
+		return w
+	}
+	return 0
+}
+
+func ParsePair(s string) map[uint64]uint64 {
+	// TODO
+	res := make(map[uint64]uint64, 0)
+	var prev, next uint64
+	for {
+		left := strings.Index(s, "(")
+		right := strings.Index(s, ")")
+		if left < right {
+			_, err := fmt.Sscanf(s[left:right+1], "(%v,%v)", &prev, &next)
+			if err == nil {
+				res[prev] = next
+			}
+		}
+		if right+1 < len(s) {
+			s = s[right+1 : len(s)]
+		} else {
+			break
+		}
+	}
+	return res
+}
+
+func ParseInput() {
+	str_pairs := os.Getenv("Input")
+	if str_pairs == "" {
+		return
+	}
+	pairs := ParsePair(str_pairs)
+	config.mu.Lock()
+	defer config.mu.Unlock()
+	for prev, next := range pairs {
+		config.sendmap[prev] = next
+		config.waitmap[next] = prev
 	}
 }
 
-func SetEnableWait(en uint64) {
-	enablewait = en
-}
-
-func SetEnableSend(en uint64) {
-	enablesend = en
-}
-
-func SetTimeout(ms int) {
-	timeout = time.Duration(ms * 1000000)
+func SetTimeout(s int) {
+	timeout = time.Second * time.Duration(s)
 }
 
 /*
 TODO : change the bitmap, to statisfy real project
 1. about fragment, we need a map to map id to its search space, if we want to use current fuzzer, the space should no bigger than 64
 */
-func InstChBF[T any](id uint64, e uint32, o chan T) {
-	if debugSched {
-		// print("chan: ", o, "; id :", id, ";\n")
-	}
-	mid := idmap.get(id)
-	if istraced&(1<<mid) != 0 {
-		// log the unmapped id
-		log.LOG.LogWithTime(id, e, o)
-	}
-	if enablewait&(1<<mid) == 0 {
+func InstChBF[T any](id uint64, o chan T) {
+	sid := config.sender(id)
+	if sid == 0 {
 		return
 	}
 	for {
-		evt := atomic.LoadUint32(&event)
-		if evt&e != 0 {
-			atomic.StoreUint32(&event, evt&(^e))
-			break
+		if _, ok := event.LoadAndDelete(sid); ok {
+			return
+		}
+		select {
+		case <-cancel:
+			return
+		default:
+
 		}
 	}
+	return
 }
 
-func InstChAF[T any](id uint64, e uint32, o chan T) {
+func InstChAF[T any | chan T | <-chan T | chan<- T](id uint64, o T) {
 	if debugSched {
 		print("[FB] chan: obj=", o, "; id=", id, ";\n")
 	}
-	mid := idmap.get(id)
-	if istraced&(1<<mid) != 0 {
-		log.LOG.LogWithTime(id, e, o)
-	}
-	if enablesend&(1<<mid) == 0 {
+	wid := config.waiter(id)
+	if wid == 0 {
 		return
 	}
-	evt := atomic.LoadUint32(&event)
-	evt |= e
-	atomic.StoreUint32(&event, evt)
+	event.Store(id, struct{}{})
 }
 
-func InstMutexBF(id uint64, e uint32, o any) {
-	if debugSched {
-		// print("mutex: ", &o, "; id :", id, ";\n")
-	}
-	mid := idmap.get(id)
-	if istraced&(1<<mid) != 0 {
-		// log the unmapped id
-		log.LOG.LogWithTime(id, e, o)
-	}
-	if enablewait&(1<<mid) == 0 {
+func InstMutexBF(id uint64, o any) {
+	sid := config.sender(id)
+	if sid == 0 {
 		return
 	}
 	for {
-		evt := atomic.LoadUint32(&event)
-		if evt&e != 0 {
-			atomic.StoreUint32(&event, evt&(^e))
-			break
+		if _, ok := event.LoadAndDelete(sid); ok {
+			return
+		}
+		select {
+		case <-cancel:
+			return
+		default:
+
 		}
 	}
+	return
 }
 
-func InstMutexAF(id uint64, e uint32, o any) {
+func InstMutexAF(id uint64, o any) {
 	if debugSched {
 		var islocked int
 		var locked bool
@@ -181,23 +158,32 @@ func InstMutexAF(id uint64, e uint32, o any) {
 		}
 		print("[FB] mutex: obj=", mid, "; id=", id, "; locked=", islocked, "; gid=", runtime.Goid(), "\n")
 	}
-	mid := idmap.get(id)
-	if istraced&(1<<mid) != 0 {
-		log.LOG.LogWithTime(id, e, o)
-	}
-	if enablesend&(1<<mid) == 0 {
+	wid := config.waiter(id)
+	if wid == 0 {
 		return
 	}
-	evt := atomic.LoadUint32(&event)
-	evt |= e
-	atomic.StoreUint32(&event, evt)
+	event.Store(id, struct{}{})
+}
+
+func GetDone() chan struct{} {
+	return make(chan struct{})
+}
+
+func GetTimeout() <-chan time.Time {
+	return time.After(timeout)
+}
+
+func Done(ch chan struct{}) {
+	close(ch)
 }
 
 func Leakcheck(t *testing.T) {
-	if event == 0 {
-		event = (1 << 32) - 1
-		time.Sleep(timeout)
-	}
+	close(cancel)
+	time.Sleep(timeout)
+	baseCheck(t)
+}
+
+func baseCheck(t *testing.T) {
 	opts := []goleak.Option{
 		goleak.IgnoreTopFunction("time.Sleep"),
 		goleak.IgnoreTopFunction("testing.(*F).Fuzz.func1"),
@@ -211,12 +197,4 @@ func Leakcheck(t *testing.T) {
 		goleak.IgnoreTopFunction("go.uber.org/goleak/internal/stack.getStackBuffer"),
 	}
 	goleak.VerifyNone(t, opts...)
-}
-
-func Check(t *testing.T) {
-	tchan := time.After(timeout)
-	select {
-	case <-tchan:
-		Leakcheck(t)
-	}
 }
